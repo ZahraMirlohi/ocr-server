d967ddf0c6888054d67c8bec87737bb5c4a33594
ZahraMirlohi <zahramirlohiiii@gmail.com>
2025-09-08 14:08:15 -0700

    Update ocr_server.py

--- ocr_server.py
+++ ocr_server.py
@@ -19,7 +19,7 @@ def ocr():
     image = Image.open(io.BytesIO(image_bytes))
 
     # استخراج متن با Tesseract
-    text = pytesseract.image_to_string(image, lang="fas+eng")
+    text = pytesseract.image_to_string(image, lang="fas+eng+ara")
 
     # پاکسازی متن برای خوانایی بهتر
     text = text.replace("\n\n", "\n")  # حذف خطوط خالی اضافی
